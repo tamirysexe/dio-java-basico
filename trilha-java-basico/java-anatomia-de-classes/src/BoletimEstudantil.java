b02248148c7d0d898c52c876300d85f5a05985c6
Tamirys Oliveira <tamirys2001@hotmail.com>
2023-09-07 20:58:01 -0300

    Boletim-Estudantil

--- trilha-java-basico/java-anatomia-de-classes/src/BoletimEstudantil.java
+++ trilha-java-basico/java-anatomia-de-classes/src/BoletimEstudantil.java
@@ -1,15 +1,12 @@
 public class BoletimEstudantil {
-    public static void main(String[] args) {
-        public class BoletimEstudantil {
-            public static void main(String[] args) {
-                int mediaFinal = 6;
-                if (mediaFinal < 6)
-                    System.out.println("REPROVADO");
-                else if (mediaFinal == 6)
-                    System.out.println("PROVA MINERVA");
-                else
-                    System.out.println("APROVADO");
-            }
-        }
+	public static void main(String[] args) {
+		int mediaFinal = 6;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
     }
-}
+
+    }
